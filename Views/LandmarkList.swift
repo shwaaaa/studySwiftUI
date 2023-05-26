a819552438d6b8a5b4bde063b1b387b018c32f82
shwaaaa <shwaa1026@naver.com>
2023-05-26 16:59:45 +0900

    studydwiftUI

--- Views/LandmarkList.swift
+++ Views/LandmarkList.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+        List(landmarks) { landmark in
+                LandmarkRow(landmark: landmark)
+            }
         }
     }
-}
+
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
